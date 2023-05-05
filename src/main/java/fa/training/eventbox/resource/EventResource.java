4044722afe81736f16683bcd653858ec1d647edb
trungluong <anzure1234@gmail.com>
2023-05-05 14:10:20 +0700

    1

--- src/main/java/fa/training/eventbox/resource/EventResource.java
+++ src/main/java/fa/training/eventbox/resource/EventResource.java
@@ -1,9 +1,13 @@
 package fa.training.eventbox.resource;
 
 import fa.training.eventbox.constant.AppConstant;
+import fa.training.eventbox.model.dto.EventListDisplayDto;
 import fa.training.eventbox.model.entity.Event;
 import fa.training.eventbox.service.EventService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class EventResource {
@@ -25,35 +30,43 @@ public class EventResource {
 
 
     @GetMapping("api/events") // HttpRequest GET /events
-    public Page<Event> showList(
-                           @RequestParam(required = false,defaultValue = AppConstant.DEFAULT_PAGE_STR) Integer page,
-                           @RequestParam(required = false,defaultValue = AppConstant.DEFAULT_PAGE_SIZE_STR) Integer size,
-                           @RequestParam(required = false,name="sort",defaultValue = AppConstant.DEFAULT_SORT_FIELD) List<String> sorts,
-                           @RequestParam(required=false,name="q") Optional<String> keywordOpt){
+    public Page<EventListDisplayDto> showList(
+            @RequestParam(required = false, defaultValue = AppConstant.DEFAULT_PAGE_STR) Integer page,
+            @RequestParam(required = false, defaultValue = AppConstant.DEFAULT_PAGE_SIZE_STR) Integer size,
+            @RequestParam(required = false, name = "sort", defaultValue = AppConstant.DEFAULT_SORT_FIELD) List<String> sorts,
+            @RequestParam(required = false, name = "q") Optional<String> keywordOpt) {
 
         List<Sort.Order> orders = new ArrayList<>();
-        for (String sortField : sorts){
+        for (String sortField : sorts) {
             boolean isDesc = sortField.startsWith("-");
-            orders.add(isDesc ? Sort.Order.desc(sortField.substring(1)):Sort.Order.asc(sortField));
+            orders.add(isDesc ? Sort.Order.desc(sortField.substring(1)) : Sort.Order.asc(sortField));
         }
 
         Specification<Event> spec = (root, query, criteriaBuilder) ->
                 criteriaBuilder.equal(root.get("deleted"), false);
-        if(keywordOpt.isPresent()){
+        if (keywordOpt.isPresent()) {
             // where name like '%keyword%' or place like '%keyword%')
-            Specification<Event> specByKeyWord=(root,query,criteriaBuilder)->
+            Specification<Event> specByKeyWord = (root, query, criteriaBuilder) ->
                     criteriaBuilder.or(
-                            criteriaBuilder.like(root.get("name"),"%"+keywordOpt.get()+"%"),
-                            criteriaBuilder.like(root.get("place"),"%"+keywordOpt.get()+"%")
+                            criteriaBuilder.like(root.get("name"), "%" + keywordOpt.get() + "%"),
+                            criteriaBuilder.like(root.get("place"), "%" + keywordOpt.get() + "%")
                     );
             //where deleted=0 and (name like '%keyword%' or place like '%keyword%')
-            spec=spec.and(specByKeyWord);
+            spec = spec.and(specByKeyWord);
         }
 
 
+        PageRequest pageRequest = PageRequest.of(page - 1, size, Sort.by(orders));
+        Page<Event> eventPage = eventService.findAllPaging(spec, pageRequest);
 
-        PageRequest pageRequest = PageRequest.of(page-1,size,Sort.by(orders));
-        Page<Event> eventPage = eventService.findAllPaging(spec,pageRequest);
-        return eventPage;
+        List<EventListDisplayDto> displayDtos = eventPage.getContent().stream()
+                .map(event -> {
+                    EventListDisplayDto eventListDisplayDto = new EventListDisplayDto();
+                    BeanUtils.copyProperties(event, eventListDisplayDto);
+                    return eventListDisplayDto;
+                }).collect(Collectors.toList());
+        //Page<Event> -> Page<EventListDisplayDto>
+        Page<EventListDisplayDto> result = new PageImpl<>(displayDtos, pageRequest, eventPage.getTotalElements());
+        return result;
     }
 }
